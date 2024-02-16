64d3152f7e50737cce5bbfb809de70f17a667f76
stephenseng <stephenseng@outlook.com>
2024-02-16 16:31:16 +0800

    Second import

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import LoginSignup from './Pages/LoginSignup';
 import Cart from './Pages/Cart';
 import Product from './Pages/Product';
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kids_banner from './Components/Assets/banner_kids.png';
 
 function App() {
   return (
@@ -15,11 +18,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Shop/>} />
-        <Route path="/men" element={<ShopCategory category="men" />} />
-        <Route path="/women" element={<ShopCategory category="women" />} />
-        <Route path="/kids" element={<ShopCategory category="kids" />} />
+        <Route path="/men" element={<ShopCategory banner={men_banner} category="men" />} />
+        <Route path="/women" element={<ShopCategory banner={women_banner} category="women" />} />
+        <Route path="/kids" element={<ShopCategory banner={kids_banner} category="kid" />} />
         <Route path="product" element={<Product />}>
-          <Route path=':productID' element={<Product />} />
+          <Route path=':productId' element={<Product />} />
         </Route>
         <Route path="/cart" element={<Cart/>} />
         <Route path="/login" element={<LoginSignup/>} />
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { ShopContext } from '../Context/ShopContext';
+import Breadcrumb from '../Components/Breadcrumbs/Breadcrumb';
+import { useParams } from 'react-router-dom';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
+    
+    const {all_product} = useContext(ShopContext);
+    const {productId} = useParams();
+    const product = all_product.find((e)=> e.id === Number(productId));
+
     return (
         <div>
-            
+            <Breadcrumb product={product} />
+            <ProductDisplay product={product} />
         </div>
     )
 }
