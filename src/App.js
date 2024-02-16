d76516beda1dbcfacb109f58ff7fa01bc15d121d
stephenseng <stephenseng@outlook.com>
2024-02-16 11:30:25 +0800

    Initial import

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Shop} from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import LoginSignup from './Pages/LoginSignup';
+import Cart from './Pages/Cart';
+import Product from './Pages/Product';
+import Footer from './Components/Footer/Footer';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop/>} />
+        <Route path="/men" element={<ShopCategory category="men" />} />
+        <Route path="/women" element={<ShopCategory category="women" />} />
+        <Route path="/kids" element={<ShopCategory category="kids" />} />
+        <Route path="product" element={<Product />}>
+          <Route path=':productID' element={<Product />} />
+        </Route>
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/login" element={<LoginSignup/>} />
+      </Routes>
+      <Footer />
+      </BrowserRouter>
+      
+    </>
+  );
+}
+
+export default App;
